d1143c4a93ccede9f4d82faeeb33eeb039af38f0
човаще <55867948+vkate174@users.noreply.github.com>
2019-10-19 08:27:41 +0400

    1

---
+++ высоцкая.py
@@ -0,0 +1,9 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> S = "2019-10-19"
+>>> date = S.split ("-")
+>>> print (date)
+['2019', '10', '19']
+>>> print (date[2] + "/" + date [1] + "/" + date [0] )
+19/10/2019
+>>> 
