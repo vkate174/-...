d08046c8936f3f7711cedf7f27d72d907b4eb818
човаще <55867948+vkate174@users.noreply.github.com>
2019-11-01 07:39:30 +0400

    Add files via upload

---
+++ lin2.py
@@ -0,0 +1,10 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> A=12
+>>> B=10
+>>> C=55
+>>> print("Max -",max(A,B,C))
+Max - 55
+>>> print("Min -",min (A,B,C))
+Min - 10
+>>> 
