e6ff4f64ab568d8b1eb8a0a1144e9cd99e1d9036
човаще <55867948+vkate174@users.noreply.github.com>
2019-11-06 22:23:37 +0500

    Add files via upload

---
+++ vov12.py
@@ -0,0 +1,9 @@
+import random
+a=random.uniform (-999,999)
+if a > 0:
+    print(str(a) + "-" + "Положительное число")
+  elif a<0:
+      print(str(a) + "-" + "Отрицательное число")
+else:
+    print(str(a) + "-" + "Число 0")
+    
