58b410ded727ae2bac23bb4a7caf798c2fa33cbf
човаще <55867948+vkate174@users.noreply.github.com>
2019-11-06 20:41:30 +0500

    Add files via upload

---
+++ vov7.py
@@ -0,0 +1,11 @@
+import random
+a=random.randint (0,999)
+print (a)
+if a % 2 ==0:
+    if a % 4 ==0:
+        print("Число чётное и кратно 4")
+      else:
+          print("Число чётное")
+else:
+    print ("Число нечётное")
+    
