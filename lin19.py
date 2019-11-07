d1e11deaddfff215383ed67b38cbbf001a3f2042
човаще <55867948+vkate174@users.noreply.github.com>
2019-11-07 21:16:10 +0500

    Add files via upload

---
+++ lin19.py
@@ -0,0 +1,13 @@
+x=10
+y=11
+z=5
+if x+y>z and a-y<z:
+    print ("exists")
+ else:
+     print ("does not exist")
+if z**2 == y**2+y**2:
+    print ("rectangular")
+ else:
+     print ("not rectangular")
+     
+     
