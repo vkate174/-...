fc7ca1d6433ac5b8a731472a4e958f492a01a826
човаще <55867948+vkate174@users.noreply.github.com>
2019-11-07 20:04:42 +0500

    Add files via upload

---
+++ lin15.py
@@ -0,0 +1,12 @@
+import random
+X = random.randint (1,54)
+print (X)
+if X>35:
+    print ("side")
+  else:
+      print ("coupe")
+if X % 2 ==0:
+    print ("top")
+ else:
+     print ("bottom")
+     
